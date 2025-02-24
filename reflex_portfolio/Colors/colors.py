e6fee88d62196f9d8cd34b3a1d926fc49f16f57c
Angel Sanchez <7294ashura49994@gmail.com>
2025-02-24 02:03:29 +0000

    starting process

--- reflex_portfolio/Colors/colors.py
+++ reflex_portfolio/Colors/colors.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class Colors(Enum):
-    navbar=""
-    heading=""
-    background=""
-    footer=""
-    text=""
+    navbar="#20210f"
+    heading="#cecec6"
+    background="#323141"
+    footer="#212111"
+    text="#8b8b8b"
