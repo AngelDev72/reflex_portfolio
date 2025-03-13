e43c05abb8d5147b4be1d48731375fd8f8e5d975
Angel Sanchez <7294ashura49994@gmail.com>
2025-03-13 21:45:14 +0000

    edited footer

--- reflex_portfolio/Components/Footer.py
+++ reflex_portfolio/Components/Footer.py
@@ -1,12 +1,12 @@
 import reflex as rx
-from reflex_portfolio.Colors.colors import Colors
+from reflex_portfolio.Colors.Colors import Colors
 
 def Footer() -> rx.Component:
-    return rx.container(
+    return rx.box(
         rx.vstack(
         rx.text("Coding from México to the world!")
         ),
         width="100%",
         margin_y="15px",
-        bg=Colors.footer.value
+        bg=Colors.Footer.value
     )
