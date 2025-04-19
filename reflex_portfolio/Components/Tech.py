f47c6897260ea55f48ee12616e700cb075c08d95
Angel Sanchez <7294ashura49994@gmail.com>
2025-04-19 22:06:52 +0000

    restructuring tech

--- reflex_portfolio/Components/Tech.py
+++ reflex_portfolio/Components/Tech.py
@@ -3,76 +3,85 @@ from reflex_portfolio.Components.Image import Tech_Image as Tech
 
 
 def Tech_Stack() -> rx.Component:
-    return rx.flex(
-        rx.vstack(
+    return rx.hstack(
+        rx.text(
+            "Language"
+            ),
             rx.hstack(
-                rx.box(
-                    rx.vstack(
-                        rx.text(
-                            "Language"
-                        ),
-                        rx.hstack(
-                            Tech(
-                                "/Img/Language/python-wordmark.png",
-                                "50",
-                                "50"
-                            ),
-                            Tech(
-                                "/Img/Language/rust.png",
-                                "50",
-                                "50"
-                            ),
-                            Tech(
-                                "/Img/Language/Mojo.png",
-                                "50",
-                                "55"
-                            )
-                        ),
-                        rx.hstack(
-                            Tech(
-                                "/Img/Language/css3.png",
-                                "50",
-                                "50"
-                            ),
-                            Tech(
-                                "/Img/Language/html5.png",
-                                "50",
-                                "50"
-                            )
-                        )
+                Tech(
+                    "/Img/Language/python-wordmark.png",
+                    "50",
+                    "50"
+                    ),
+                Tech(
+                    "/Img/Language/rust.png",
+                    "50",
+                    "50"
+                    ),
+                Tech(
+                    "/Img/Language/Mojo.png",
+                    "50",
+                    "55"
                     )
                 ),
-                rx.spacer(),
-                rx.box(
-                    rx.vstack(
-                        rx.text(
-                            "Frameworks"
-                        ),
-                        rx.hstack(
-                            Tech(
-                                "/favicon.ico",
-                                "50",
-                                "50"
-                            ),
-                            Tech(
-                                "/Img/Framework/tensorflow.png",
-                                "50",
-                                "50"
-                            ),
-                            Tech(
-                                "/Img/Framework/pandas.png",
-                                "50",
-                                "50"
-                            )
-                        )
+            rx.hstack(
+                Tech(
+                    "/Img/Language/css3.png",
+                    "50",
+                    "50"
                     ),
-                ),
+                Tech(
+                    "/Img/Language/html5.png",
+                    "50",
+                    "50"
+                    )
+                )
+            ),
+            rx.hstack(
+                rx.vstack(
+                    rx.text(
+                        "Frameworks"
+                    ),
+                rx.hstack(
+                    Tech(
+                        "/favicon.ico",
+                        "50",
+                        "50"
+                        ),
+                    Tech(
+                        "/Img/Framework/tensorflow.png",
+                        "50",
+                        "50"
+                        ),
+                    Tech(
+                        "/Img/Framework/pandas.png",
+                        "50",
+                        "50"
+                        )
+                )
                 rx.spacer(),
                 rx.box(
                     rx.text(
                         "Technology"
+                        )
                     )
-                )
             )
-        ),width="100%"
-    )
+        )
+
+def Tech() -> rx.Component:
+    return rx.vstack(
+            rx.rhstack(
+                rx.text(
+                    "Language"
+                    ),
+                    rx.spacer(spacing="4"),
+                rx.text(
+                    "Framework"
+                    ),
+                    rx.spacer(spacing="4"),
+                rx.text(
+                    "Technology"
+                    ),
+                    rx.spacer(spacing="4")
+                )
+        )
