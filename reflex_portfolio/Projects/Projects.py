e097e2dddca18711b2da3ff4c47dcbb4f4d59bc1
AngelDev72 <7294ashura49994@gmail.com>
2025-04-30 17:01:35 -0600

    added projects section

--- reflex_portfolio/Projects/Projects.py
+++ reflex_portfolio/Projects/Projects.py
@@ -1,12 +1,41 @@
 import reflex as rx
-from .Components.Navbar import Navbar
-from .Components.Avatar import Angel
 
-
-def Project() -> rx.Component:
-    return rx.container(
-        Navbar(),
+def Pymon() -> rx.Component:
+    return rx.flex(
         rx.vstack(
-            Angel(),
+            rx.text(
+                "Pymon:"
+                ),
+            rx.text(
+                """
+                Game design to have an inluence of AI later on the go. Pymon introduces 130 species of Pymons.
+                        You may notice some similarity to Pokémon, but I tried to make it the most original as posible without making it too similar also.
+                """
+                )
+            )
+        )
+
+def EON() -> rx.Component:
+    return rx.flex(
+        rx.vstack(
+            rx.text(
+                """
+                Eon is basicly an AI Model I'm trying to develop on a basis of Psychology, Philosophy, Ethics and Morality.
+                        The purpose of this project is to try and see if an AI Model can take better decisions once it's been trained on a more complex decision system.
+                """
+                )
+            )
+        )
+
+def ChickNTec() -> rx.Component:
+    return rx.flex(
+        rx.vstack(
+            rx.center(
+                rx.text(
+                    """
+                    
+                    """
+                    )
+                )
             )
         )
