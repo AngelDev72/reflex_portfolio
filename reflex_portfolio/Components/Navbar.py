b6112eb0eaa76762606d0bcb82450c3c7acbae84
AngelDev72 <7294ashura49994@gmail.com>
2025-09-20 21:46:30 -0600

    fixed minor setback

--- reflex_portfolio/Components/Navbar.py
+++ reflex_portfolio/Components/Navbar.py
@@ -4,55 +4,11 @@ from ..Colors.Colors import Colors
 
 
 def Navbar(active_page: str) -> rx.Component:
-    pages={
-        "AngelDev":[
-            (
-                "/projects",
-                "Projects",
-                ),
-            (
-                "/about",
-                "About"
-                )
-            ],
-        "About":[
-            (
-                "/",
-                "AngelDev"
-                ),
-            (
-                "/projects",
-                "Projects",
-                )
-            ],
-        "Projects":[
-            (
-                "/",
-                "AngelDev",
-                ),
-            (
-                "/about",
-                "About"
-                )
-            ]
-    }
-    links=[
-        rx.link(
-            rx.text(label),
-            href=path,
-            style={"text_decoration"=="none"},
-            )
-            for group in pages.values()
-            for path, label in group
-            if path != f"/{active_page}" and not (active_page=="AngelDev" and path=="/")
-        ]
-        
     return rx.hstack(
         rx.text(
             "AngelDev 🐍"
         ),
         rx.spacer(spacing="2"),
-        *links,
         top="0",
         z_index="999",
         width="100%",
